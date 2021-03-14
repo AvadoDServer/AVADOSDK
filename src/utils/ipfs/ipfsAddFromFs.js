4405e4ff5c92531c2e1aadc52826a2c7af763674
stefaan <stefaan@ponnet.com>
2021-03-14 08:14:14 +0100

    switch to ipfs-http-client for uploading

--- src/utils/ipfs/ipfsAddFromFs.js
+++ src/utils/ipfs/ipfsAddFromFs.js
@@ -4,6 +4,8 @@ const got = require("got");
 const FormData = require("form-data");
 const traverseDir = require("./traverseDir");
 const { normalizeIpfsProvider } = require("./ipfsProvider");
+const IpfsHttpClient = require('ipfs-http-client');
+const { globSource } = IpfsHttpClient;
 
 /**
  * Uploads a directory or file from the fs
@@ -15,47 +17,68 @@ const { normalizeIpfsProvider } = require("./ipfsProvider");
 async function ipfsAddFromFs(dirOrFile, ipfsProvider, onProgress) {
   // Create form and append all files recursively
 
-  const form = new FormData();
-  // Automatically detect if recursive if needed if directory
-  if (fs.lstatSync(dirOrFile).isDirectory()) {
-    const dirDir = path.parse(dirOrFile).dir;
-    const filePaths = traverseDir(dirOrFile);
-    for (const filePath of filePaths) {
-      form.append(`file-${filePath}`, fs.createReadStream(filePath), {
-        // Compute filepaths from the provided dirOrFile and below only
-        filepath: path.relative(dirDir, filePath)
-      });
-    }
-  } else {
-    // Add single files without providing a filepath
-    form.append("file", fs.createReadStream(dirOrFile));
-  }
+  // const form = new FormData();
+  // // Automatically detect if recursive if needed if directory
+  // if (fs.lstatSync(dirOrFile).isDirectory()) {
+  //   const dirDir = path.parse(dirOrFile).dir;
+  //   const filePaths = traverseDir(dirOrFile);
+  //   for (const filePath of filePaths) {
+  //     form.append(`file-${filePath}`, fs.createReadStream(filePath), {
+  //       // Compute filepaths from the provided dirOrFile and below only
+  //       filepath: path.relative(dirDir, filePath)
+  //     });
+  //   }
+  // } else {
+  //   // Add single files without providing a filepath
+  //   form.append("file", fs.createReadStream(dirOrFile));
+  // }
 
   // Parse the ipfsProvider the a full base apiUrl
   const apiUrl = normalizeIpfsProvider(ipfsProvider);
-  const res = await got({
-    prefixUrl: apiUrl,
-    url: "api/v0/add",
-    method: "POST",
-    headers: form.getHeaders(),
-    body: form
-  }).on("uploadProgress", progress => {
-    // Report upload progress
-    // { percent: 0.9995998225975282, transferred: 733675762, total: 733969480 }
-    if (onProgress) onProgress(progress.percent);
-  });
 
-  // res.body = '{"Name":"dir/file","Hash":"Qm...","Size":"2203"}\n{"Name":"dir","Hash":"Qm...","Size":"24622"}\n'
-  // Trim last \n, split entries by \n and then select the last which is the root directory
-  const lastFileUnparsed = res.body
-    .trim()
-    .split("\n")
-    .slice(-1)[0];
-  if (!lastFileUnparsed) throw Error(`No files in response body ${res.body}`);
+  const ipfs = IpfsHttpClient(new URL(apiUrl))
+
+  if (fs.lstatSync(dirOrFile).isDirectory()) {
+    // const dirDir = path.parse(dirOrFile).dir;
+    // const filePaths = traverseDir(dirOrFile);
+    // for (const filePath of filePaths) {
+    //   form.append(`file-${filePath}`, fs.createReadStream(filePath), {
+    //     // Compute filepaths from the provided dirOrFile and below only
+    //     filepath: path.relative(dirDir, filePath)
+    //   });
+    // }
+    console.log(`Directory adding is not supported`);
+    process.exit();
+  } 
+    // Add single files without providing a filepath
+    // form.append("file", fs.createReadStream(dirOrFile));
+    const {cid} = await ipfs.add(globSource('./docs', { recursive: true }))
+  
+
+
+  // const res = await got({
+  //   prefixUrl: apiUrl,
+  //   url: "api/v0/add",
+  //   method: "POST",
+  //   headers: form.getHeaders(),
+  //   body: form
+  // }).on("uploadProgress", progress => {
+  //   // Report upload progress
+  //   // { percent: 0.9995998225975282, transferred: 733675762, total: 733969480 }
+  //   if (onProgress) onProgress(progress.percent);
+  // });
+
+  // // res.body = '{"Name":"dir/file","Hash":"Qm...","Size":"2203"}\n{"Name":"dir","Hash":"Qm...","Size":"24622"}\n'
+  // // Trim last \n, split entries by \n and then select the last which is the root directory
+  // const lastFileUnparsed = res.body
+  //   .trim()
+  //   .split("\n")
+  //   .slice(-1)[0];
+  // if (!lastFileUnparsed) throw Error(`No files in response body ${res.body}`);
 
   // Parse the JSON and return the hash of the root directory
-  const lastFile = JSON.parse(lastFileUnparsed);
-  return `/ipfs/${lastFile.Hash}`;
+  // const lastFile = JSON.parse(lastFileUnparsed);
+  return `/ipfs/${cid}`;
 }
 
 module.exports = ipfsAddFromFs;
